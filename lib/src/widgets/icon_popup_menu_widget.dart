b5aead6297a5646823d5090db38644b2cc112de2
Luizzlcs <luizzlcs@gmail.com>
2024-03-19 16:19:28 -0300

    customizando icone da appBar

---
+++ lib/src/widgets/icon_popup_menu_widget.dart
@@ -0,0 +1,19 @@
+import 'package:fe_lab_clinicas_core/src/theme/lab_clinicas_theme.dart';
+import 'package:flutter/material.dart';
+
+class IconPopupMenuWidget extends StatelessWidget {
+  const IconPopupMenuWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 64),
+      child: Container(
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(color: LabClinicasTheme.orangeColor, width: 2)),
+        child: const Icon(Icons.more_horiz_rounded),
+      ),
+    );
+  }
+}
