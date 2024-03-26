4c99a8c3ebad01af2397fabdb48c4879e1470b6e
Luizzlcs <luizzlcs@gmail.com>
2024-03-26 13:41:12 -0300

    Correções

--- lib/src/restClient/interceptors/auth_interceptor.dart
+++ lib/src/restClient/interceptors/auth_interceptor.dart
@@ -3,25 +3,20 @@ import 'package:dio/dio.dart';
 import 'package:fe_lab_clinicas_core/src/constants/local_storage_constants.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-final class AuthInterceptor extends Interceptor{
-
+final class AuthInterceptor extends Interceptor {
   @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler)async {
-
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
     final RequestOptions(:headers, :extra) = options;
     const authHeaderKey = 'Authorization';
     headers.remove(authHeaderKey);
-
-    if(extra case{'DIO_AUTH_KEY': true}){
+    
+    if(extra case {'DIO_AUTH_KEY' : true}){
       final sp = await SharedPreferences.getInstance();
       headers.addAll({
-        authHeaderKey: 'Bearer ${sp.getString(LocalStorageConstants.accessToken)}',
-      });
-
+        authHeaderKey: 'Bearer ${sp.getString(LocalStorageConstants.accessToken)}'
+      });  
     }
     handler.next(options);
     
-    super.onRequest(options, handler);
   }
-  
 }
